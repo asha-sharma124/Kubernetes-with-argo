d731a4230c22a69ab3080bfd0b4b0b2ffd96bdd9
asha <ashasharma08708@google.com>
2025-11-23 22:05:56 +0530

    init correct

--- kubernetes-manifests/access-app.sh
+++ kubernetes-manifests/access-app.sh
@@ -8,3 +8,5 @@ kubectl port-forward -n argocd service/argocd-server 8443:443 &
 
 kubectl port-forward svc/argo-server -n argo 2746:2746 &
 #kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
+
+#kubectl rollout restart deployment argocd-repo-server -n argocd
