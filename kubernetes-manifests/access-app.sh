86023edf23e33a207bd007e0f30fc886b82c9767
asha <ashasharma08708@google.com>
2025-11-23 21:51:41 +0530

    init containers

--- kubernetes-manifests/access-app.sh
+++ kubernetes-manifests/access-app.sh
@@ -7,3 +7,4 @@ kubectl port-forward svc/gateway 7070:80 -n piggymetrics &
 kubectl port-forward -n argocd service/argocd-server 8443:443 &
 
 kubectl port-forward svc/argo-server -n argo 2746:2746 &
+#kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
